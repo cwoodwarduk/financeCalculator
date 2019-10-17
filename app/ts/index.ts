9a0d1edf4cf3bdf7f4749169d51f1339ea3f97c2
cwoodwarduk <calum.james.woodward@gmail.com>
2019-10-17 15:36:20 +0100

    set up of gulp, scss, typescript and associated files

---
+++ app/ts/index.ts
@@ -0,0 +1,10 @@
+var preenyabluh: number = 42
+var smee : string = 'hee'
+
+function nonsense(thing1: number, thing2: string) {
+    var offDuty: Array<any> = []
+    offDuty.push(thing1, thing2)
+    console.log(offDuty)
+}
+
+nonsense(preenyabluh, smee)
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var ts = require('gulp-typescript')
+var tsProject = ts.createProject('tsconfig.json')
+
+function typey(){
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(gulp.dest('app/js/typescripted'))
+}
+
+function sassCompile(cb){
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass()) //convert sass to css with gulp sass
+        .pipe(gulp.dest('app/css'))
+    cb()
+}
+
+function watch(){
+    gulp.watch('app/scss/**/*.scss', sassCompile)
+    gulp.watch('app/ts/**/*.ts', typey)
+}
+
+
+exports.sass = sassCompile
+exports.watch = watch
+exports.typey = typey
