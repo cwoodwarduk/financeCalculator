96897180ff6185cf0397bc0e314034584d00aa96
cwoodwarduk <calum.james.woodward@gmail.com>
2019-10-17 16:12:38 +0100

    set up basic variables and event listener to handle form inputs

---
+++ app/js/typescripted/index.js
@@ -0,0 +1,12 @@
+var inputs = $('.inputField');
+var amount = $('#amount');
+var salary = $('#expectedSalary');
+var repayment = $('#repayment');
+inputs.addEventListener('input', function (e) {
+    if (e.target == amount) {
+    }
+    else if (e.target == salary) {
+    }
+    else if (e.target == repayment) {
+    }
+});
